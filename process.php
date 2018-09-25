2c0bcd668128e5a7673f0226ece180b71750b76f
Atita <anaguasoft@gmail.com>
2018-09-26 01:13:49 +0800

    ajax fetching with PHP

--- process.php
+++ process.php
@@ -1,9 +1,24 @@
 <?php
 
+$conn = mysqli_connect('localhost', 'root','','ajaxtest');
+
+
+
+
 echo 'Processing...';
 
 if (isset($_POST['name'])) {
+	$name = mysqli_real_escape_string($conn,$_POST['name']);
 	echo 'POST: Your name is '. $_POST['name'];
+	
+	$query ="INSERT INTO users(name) VALUES('$name')";
+	
+	if(mysqli_query($conn,$query)) {
+		echo 'User Added...';
+		} else {
+			echo 'ERROR: ' . mysqli_error($conn);
+		}
+	
 }
 
 
